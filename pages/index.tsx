18bf6853be85fb570a1c3f91226459113f79daf0
torruxp <92608571+torruxp@users.noreply.github.com>
2022-04-22 20:26:36 -0300

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Questao from '../components/Questao'
+import QuestaoModel from '../model/questao'
+import RespostaModel from '../model/resposta'
+
+
+
+  const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
+    RespostaModel.errada('Verde'),
+    RespostaModel.errada('Vermelha'),
+    RespostaModel.errada('Azul'),
+    RespostaModel.certa('Preta'),
+  ])
+
+  export default function Home(){
+    const [questao, setQuestao] = useState(questaoMock)
+  
+
+  function respostaFornecida(indice: number){
+    setQuestao(questao.responderCom(indice))
+  }
+
+  function tempoEsgotado(){
+    setQuestao(questao.responderCom(-1))
+  }
+
+  return (
+    <div style= {{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <Questao valor={questao} respostaFornecida={respostaFornecida}
+      tempoEsgotado = {tempoEsgotado}/>
+    </div>
+  )
+}
