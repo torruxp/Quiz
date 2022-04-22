acd0f02ea184176b325b6b503aaa4981b211a965
torruxp <92608571+torruxp@users.noreply.github.com>
2022-04-22 20:41:55 -0300

    Delete questionario.ts

--- pages/api/questionario.ts
+++
@@ -1,7 +0,0 @@
-import { embaralhar } from "../../../function/arrays"
-import questoes from "../bancoDeQuestoes"
-
-export default(req, res) => {
-    const ids= questoes.map(questao => questao.id)
-res.status(200).json(embaralhar(ids))
-}
